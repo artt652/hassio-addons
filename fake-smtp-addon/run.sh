572fdf44ab06d444126f42b493e814b2ae01a7d7
artt652 <62139160+artt652@users.noreply.github.com>
2024-09-21 23:59:51 +0500

    Update run.sh

--- fake-smtp-addon/run.sh
+++ fake-smtp-addon/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Export the environment variables from Home Assistant's add-on configuration
-export TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN
-export TELEGRAM_CHAT_ID=$TELEGRAM_CHAT_ID
+# Read environment variables from Home Assistant Add-on configuration
+export TELEGRAM_BOT_TOKEN="$TELEGRAM_BOT_TOKEN"
+export TELEGRAM_CHAT_ID="$TELEGRAM_CHAT_ID"
+
+# Check if the required environment variables are set
+if [ -z "$TELEGRAM_BOT_TOKEN" ] || [ -z "$TELEGRAM_CHAT_ID" ]; then
+  echo "Error: Missing TELEGRAM_BOT_TOKEN or TELEGRAM_CHAT_ID environment variables."
+  exit 1
+fi
 
 # Start the fake SMTP server
 python3 /usr/src/app/smtp_server.py
