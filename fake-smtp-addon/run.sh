ae5cafcaeadfb301b2a26ec7ae7e74a8fb7d031b
artt652 <62139160+artt652@users.noreply.github.com>
2024-09-21 16:43:03 +0500

    Update run.sh

--- fake-smtp-addon/run.sh
+++ fake-smtp-addon/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Export the options from Home Assistant configuration
-export TELEGRAM_BOT_TOKEN="${TELEGRAM_BOT_TOKEN}"
-export TELEGRAM_CHAT_ID="${TELEGRAM_CHAT_ID}"
+# Export the environment variables from Home Assistant's add-on configuration
+export TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN
+export TELEGRAM_CHAT_ID=$TELEGRAM_CHAT_ID
 
-# Run the Python SMTP server
+# Start the fake SMTP server
 python3 /usr/src/app/smtp_server.py
